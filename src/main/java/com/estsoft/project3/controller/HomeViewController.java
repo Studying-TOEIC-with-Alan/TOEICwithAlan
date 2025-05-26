6be6d0fd6f0d1f89c8732bdac9ae3def35c7966a
elini_ng <huangenni@yahoo.com>
2025-05-26 12:08:01 +0900

    home page

---
+++ src/main/java/com/estsoft/project3/controller/HomeViewController.java
@@ -0,0 +1,12 @@
+package com.estsoft.project3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeViewController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
