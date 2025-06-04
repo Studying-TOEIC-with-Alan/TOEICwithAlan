7b4ad0167c4318a4a2280e85746186291f3a5468
NohYeongJae <nyj1665@naver.com>
2025-06-04 13:52:33 +0900

    #   refactor   : 브랜치 변경으로 인한 커밋

--- src/main/java/com/estsoft/project3/repository/UserRepository.java
+++ src/main/java/com/estsoft/project3/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.estsoft.project3.repository;
 
 import com.estsoft.project3.domain.User;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUserIdAndIsActive(Long userId, String isActive);
-    User findByNickname(String nickname);
+    Page<User> findByNicknameContaining(String keyword, Pageable pageable);
+    Page<User> findAll(Pageable pageable);
 }
