504d68f7b205f2de3cebede562ccaf67850c7a29
elini_ng <huangenni@yahoo.com>
2025-06-05 13:02:02 +0900

    1. Update http session when nickname is updated 2. Index.js code tidy up

--- src/main/java/com/estsoft/project3/dto/SessionUser.java
+++ src/main/java/com/estsoft/project3/dto/SessionUser.java
@@ -4,15 +4,17 @@ import com.estsoft.project3.domain.Role;
 import com.estsoft.project3.domain.User;
 import java.io.Serializable;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class SessionUser implements Serializable {
 
-    private final String nickname;
-    private final Role role;
-    private final Long score;
+    private String nickname;
+    private Role role;
+    private Long score;
     private final Long userId;
-    private final Long grade;
+    private Long grade;
 
     public SessionUser(User user){
         this.nickname = user.getNickname();
