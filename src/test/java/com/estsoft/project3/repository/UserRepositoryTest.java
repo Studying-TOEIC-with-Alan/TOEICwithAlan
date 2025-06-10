cff053120bb48a56d96b4a15049549e06ec55d7a
Elini Ng <huangenni@yahoo.com>
2025-06-10 10:09:12 +0900

    1. Add findByNicknameContaining and findAll test of UserRepository 2. Modify TilRepositoryTest to check paging related items 3. Amend typo in sql.txt

--- src/test/java/com/estsoft/project3/repository/UserRepositoryTest.java
+++ src/test/java/com/estsoft/project3/repository/UserRepositoryTest.java
@@ -5,8 +5,12 @@ import com.estsoft.project3.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,20 +22,20 @@ class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
-    private User createTestUser() {
+    private User createTestUser(String provider, String email, String nickname, String isActive) {
         User user = new User();
-        user.setProvider("google");
-        user.setEmail("email@test.com");
-        user.setNickname("Nickname");
+        user.setProvider(provider);
+        user.setEmail(email);
+        user.setNickname(nickname);
         user.setRole(Role.ROLE_USER);
-        user.setIsActive("Y");
+        user.setIsActive(isActive);
         return userRepository.save(user);
     }
 
     @Test
     void findByEmail() {
         //given:
-        User savedUser = createTestUser();
+        User savedUser = createTestUser("google", "email@test.com", "Nickname", "Y");
 
         //when:
         Optional<User> userInfo = userRepository.findByEmail("email@test.com");
@@ -48,7 +52,7 @@ class UserRepositoryTest {
     @Test
     void findByUserIdAndIsActive() {
         //given:
-        User savedUser = createTestUser();
+        User savedUser = createTestUser("google", "email@test.com", "Nickname", "Y");
 
         //when:
         Optional<User> userInfo = userRepository.findByUserIdAndIsActive(savedUser.getUserId(), "Y");
@@ -65,7 +69,7 @@ class UserRepositoryTest {
     @Test
     void findByNickname() {
         //given:
-        User savedUser = createTestUser();
+        User savedUser = createTestUser("google", "email@test.com", "Nickname", "Y");
 
         //when:
         User userInfo = userRepository.findByNickname("Nickname");
@@ -77,5 +81,50 @@ class UserRepositoryTest {
         assertEquals(savedUser.getNickname(), userInfo.getNickname());
         assertEquals(savedUser.getRole(), userInfo.getRole());
         assertEquals(savedUser.getIsActive(), userInfo.getIsActive());
+    }
+
+    @Test
+    void findByNicknameContaining() {
+        //given:
+        User savedUser1 = createTestUser("google", "email1@test.com", "Nickname1", "Y");
+        User savedUser2 = createTestUser("kakao", "email2@test.com", "OtherName1", "Y");
+        User savedUser3 = createTestUser("facebook", "email2@test.com", "OtherName2", "Y");
+        Pageable pageable = PageRequest.of(0, 10);
+
+        //when:
+        Page<User> userPage = userRepository.findByNicknameContaining("Nickname", pageable);
+
+        //then:
+        assert userPage != null;
+        assertEquals(1,userPage.getTotalPages());
+        assertEquals(1,userPage.getTotalElements());
+
+        List<User> userList = userPage.getContent();
+        assertEquals(savedUser1.getProvider(), userList.get(0).getProvider());
+        assertEquals(savedUser1.getEmail(), userList.get(0).getEmail());
+        assertEquals(savedUser1.getNickname(), userList.get(0).getNickname());
+    }
+
+    @Test
+    void findAll() {
+        //given:
+        User savedUser1 = createTestUser("google", "email1@test.com", "Nickname1", "Y");
+        User savedUser2 = createTestUser("kakao", "email2@test.com", "Nickname2", "Y");
+        User savedUser3 = createTestUser("facebook", "email3@test.com", "Nickname3", "Y");
+        Pageable pageable1 = PageRequest.of(0, 10);
+        Pageable pageable2 = PageRequest.of(1, 2);
+
+        //when:
+        Page<User> userPage1 = userRepository.findAll(pageable1);
+        Page<User> userPage2 = userRepository.findAll(pageable2);
+
+        //then:
+        assertEquals(1,userPage1.getTotalPages());
+        assertEquals(3,userPage1.getTotalElements());
+        assertEquals(3,userPage1.getContent().size());
+
+        assertEquals(2,userPage2.getTotalPages());
+        assertEquals(3,userPage2.getTotalElements());
+        assertEquals(1,userPage2.getContent().size());
     }
 }
